fe8c7b1ebf4804fe0eb2f00c5beba3a3eba363cc
Eprst <easter_p@mail.ru>
2022-05-18 23:06:26 +0300

    первичная работоспособность

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,51 +1,47 @@
 package org.example;
 
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        //это urlfixed, но можно использовать и обычный: "jdbc:mysql://localhost:3306/garden" (не уверен, нужно ли явно у4казывать 3306 тут)
-        String url = "jdbc:mysql://localhost:3306/garden?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true\" +\n" +
-                "                    \"&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String url = "jdbc:mysql://localhost:3306/garden";
         String username = "root";
         String password = "dnoliver123";
 
-        Connection connection;
         try{
-            //Driver driver = new com.mysql.cj.jdbc.Driver();
-            //DriverManager.registerDriver(driver);
-            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            connection = DriverManager.getConnection(url, username, password);
-            //Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+
+            //statement.execute("INSERT INTO gardentable (location, carrot, potato, cabbage) VALUES ('birds', 12, 4, 3);");
+            //statement.executeUpdate("UPDATE gardentable SET carrot=15 WHERE location='rabbits'");
+            ResultSet selectAll = statement.executeQuery("SELECT * FROM gardentable");
+
+            while (selectAll.next()) {
+                String location = selectAll.getString("location");
+                System.out.println(location);
+            }
+
+//            statement.addBatch("INSERT INTO gardentable (location, carrot, potato, cabbage) VALUES ('house', 42, 223, 27)");
+//            statement.addBatch("INSERT INTO gardentable (location, carrot, potato, cabbage) VALUES ('garden bed 1', 12, 45, 8)");
+//            statement.addBatch("INSERT INTO gardentable (location, carrot, potato, cabbage) VALUES ('garden bed 2', 10, 37, 6)");
+//            statement.executeBatch();
+//            statement.clearBatch();
+
 
             if (!connection.isClosed()) {
                 System.out.println("Connection success!");
             }
+
+            connection.close();
+
+            if (connection.isClosed()) {
+                System.out.println("Connection close!");
+            }
         }
         catch(SQLException ex){
             System.out.println("Connection failed...");
-
             System.out.println(ex);
-
-
-
-
-
-            //авто кетчи при фиксе ошибок компиляции, пока пусть висят, потом пофиксить
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
+       }
     }
 }
